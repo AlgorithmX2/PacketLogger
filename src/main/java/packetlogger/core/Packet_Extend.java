1ecf1da45586823f48ddae705c65299ac0c4d44e
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-09-09 19:36:51 -0500

    Log things after you read them, its way more productive. Also show bytes in custom payloads, thats helpful too..

--- src/main/java/packetlogger/core/Packet_Extend.java
+++ src/main/java/packetlogger/core/Packet_Extend.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketBuffer;
 
-
-public abstract class Packet_Extend extends Packet {
+public abstract class Packet_Extend extends Packet
+{
 
 	@PacketLoggerCopy
 	public void overridden_readPacketData(PacketBuffer thing) throws IOException
 	{
-		PacketLogger.logPacket( (Packet)this );
-		this.readPacketData(thing);
+		this.readPacketData( thing );
+		PacketLogger.logPacket( (Packet) this );
 	}
-	
+
 }
